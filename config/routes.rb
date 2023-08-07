2b721ee771bcaaec8577232f4c4b8c17ecdef577
Devi Putri <devi.putri@live.com>
2023-08-07 13:41:28 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/answer', to: 'questions#answer', as: :answer
+  root to: 'questions#ask'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
